e997b82610fd946b7e6ed6adbf9e7a6f0d437445
parakeet15 <kaisei_19n1102563@nnn.ed.jp>
2020-12-05 18:46:38 +0900

    カーソルを使用する

--- index.js
+++ index.js
@@ -39,29 +39,29 @@ window.onload = () => {
     }
 
     // 保存されているデータを一覧に表示する
-    const transaction = database.transaction(storeName, 'readonly');
-    const objectStore = transaction.objectStore(storeName);
+    // const transaction = database.transaction(storeName, 'readonly');
+    // const objectStore = transaction.objectStore(storeName);
 
-    const countRequest = objectStore.count();
-    countRequest.onsuccess = e => {
-      if (e.target.result === 0) {
-        console.info('データがありません');
-        return;
-      }
+    // const countRequest = objectStore.count();
+    // countRequest.onsuccess = e => {
+    //   if (e.target.result === 0) {
+    //     console.info('データがありません');
+    //     return;
+    //   }
 
-      const getAllKeysRequest = objectStore.getAllKeys();
-      getAllKeysRequest.onsuccess = e => {
-        const Keys = e.target.result;
-        Keys.forEach(id => output(id));
-      }
-    }
+    //   const getAllKeysRequest = objectStore.getAllKeys();
+    //   getAllKeysRequest.onsuccess = e => {
+    //     const Keys = e.target.result;
+    //     Keys.forEach(id => output(id));
+    //   }
+    // }
 
     // カーソルを使用する
     const cursorRequest = objectStore.openCursor(null, 'prev');
-    cursorRequest.onsuccess = evt => {
-      const cursor = evt.target.result;
+    cursorRequest.onsuccess = e => {
+      const cursor = e.target.result;
       if (cursor === null) return;
-      console.dir(cursor.value);
+      output(cursor.value.id);
       cursor.advance(1);
     }
   }
